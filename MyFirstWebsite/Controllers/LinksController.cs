8ea1b2c42f428c15dd3a3c516704ee3728cc8ea3
Gaurav Mishra <gauravmishra.hotmail@gmail.com>
2018-10-02 20:09:12 +0530

    Add project files.

---
+++ MyFirstWebsite/Controllers/LinksController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstWebsite.Controllers
+{
+    //public acess modifier
+    //:controller name of class and links controller is intheritated from controller class
+
+    public class LinksController : Controller
+    {
+        // GET: Links
+        //public means 
+        //return type is action results
+
+        public ActionResult Index()
+        {
+            //render view 
+            return View();
+        }
+    }
+}
